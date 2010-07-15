c46212c6bd4151dd5dc37f9bc66a9f7cbeeea792
Camilo Aguilar <camilo.aguilar@gmail.com>
2010-07-15 09:10:51 -0400

    writing the README

--- example.js
+++ example.js
@@ -50,5 +50,5 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
 }).listen(8124);
-console.log('iniciando http server')
+console.log('Http server started');
 
