6ef65ba40f4d76f315a4363ee1f359034b3b906c
Camilo Aguilar <camilo.aguilar@gmail.com>
2011-11-11 15:14:05 -0500

    bumps version

--- src/node_inotify.h
+++ src/node_inotify.h
@@ -10,7 +10,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define NODE_INOTIFY_VERSION "0.2.1"
+#define NODE_INOTIFY_VERSION "0.2.2"
 
 using namespace v8;
 using namespace node;
