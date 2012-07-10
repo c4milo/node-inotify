b8aebdaa107b9e18fd866b869d899056715e6328
Camilo Aguilar <camilo.aguilar@gmail.com>
2012-07-09 23:30:39 -0400

    Minor change

--- inotify.js
+++ inotify.js
@@ -1,5 +1,5 @@
 try {
-    module.exports = require('./build/Debug/inotify');
-} catch(e) {
     module.exports = require('./build/Release/src/inotify');
+} catch(e) {
+    module.exports = require('./build/Debug/inotify');
 }
