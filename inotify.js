3aedead72bc12845e8df59bb7df2b88fc8fd5988
Camilo Aguilar <camilo.aguilar@gmail.com>
2013-01-18 10:10:51 -0500

    Fixes path to released binary

--- inotify.js
+++ inotify.js
@@ -1,5 +1,5 @@
 try {
-    module.exports = require('./build/Release/src/inotify');
+    module.exports = require('./build/Release/inotify');
 } catch(e) {
     module.exports = require('./build/Debug/inotify');
 }
